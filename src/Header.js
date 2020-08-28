ae456966e680ca88439a72827cbcaa7a3ebbf11a
Vikas Choubey <vikas.cby@gmail.com>
2020-08-28 22:40:11 +0530

    add search bar and icon

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
+import "./Header.css";
 
 
 const Header = () => {
@@ -7,6 +9,11 @@ const Header = () => {
         <Link to='/'>
             <img className="logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt='Logo'></img>
         </Link>
+        <div className='header-search'>
+            <input name="search-bar" className="search-bar"></input>
+            <SearchIcon className='search-icon'></SearchIcon>
+        </div>
+
     </nav>
 }
 
