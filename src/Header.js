9cf41ea784a0ff8f12eb2206fe77db4fb241c6d7
Vikas Choubey <vikas.cby@gmail.com>
2020-08-29 13:22:09 +0530

    add cart and counter, minor style updates

--- src/Header.js
+++ src/Header.js
@@ -1,35 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import "./Header.css";
 
 
 const Header = () => {
-    return <nav className='header'>
+    return <nav className='nav'>
+    {/* Amazon Logo */}
         <Link to='/'>
             <img className="logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt='Logo'></img>
         </Link>
-        <div className='header-search'>
+       
+    {/* Search Bar  */}
+        <div className='nav-search'>
             <input name="search-bar" className="search-bar" placeholder='search for items'></input>
             <SearchIcon className='search-icon'></SearchIcon>
         </div>
 
-        <Link to='/login' className='header-link'>
-            <div className='header-link-options'>
-            <span>Hello,</span>
-            <span>Sign In</span>
+    {/* Links */}
+
+        {/* Login  */}
+        <Link to='/login' className='nav-link'>
+            <div className='nav-link-options'>
+                <span className='nav-link-option-top'>Hello,</span>
+                <span className='nav-link-option-bottom'>Sign In</span>
             </div>
         </Link>
-        <Link to='/orders' className='header-link'>
-        <div className='header-link-options'>
-            <span>Returns</span>
-            <span>& Orders</span>
+
+        {/* Orders */}
+        <Link to='/orders' className='nav-link'>
+            <div className='nav-link-options'>
+                <span className='nav-link-option-top'>Returns</span>
+                <span className='nav-link-option-bottom'>& Orders</span>
             </div>
         </Link>
-        <Link to='/prime' className='header-link'>
-        <div className='header-link-options'>
-            <span>Your</span>
-            <span>Prime</span>
+
+        {/* Prime */}
+        <Link to='/prime' className='nav-link'>
+            <div className='nav-link-options'>
+                <span className='nav-link-option-top'>Your</span>
+                <span className='nav-link-option-bottom'>Prime</span>
+            </div>
+        </Link>
+
+        {/* Cart & Counter */}
+        <Link to='/cart' className='nav-link'>
+            <div className='nav-link-cart'>
+                <ShoppingCartIcon ></ShoppingCartIcon>
+                <div className='item-count'>0</div>
             </div>
         </Link>
 
