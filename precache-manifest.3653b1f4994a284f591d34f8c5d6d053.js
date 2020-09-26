d31d3815a6f87b0a1e3ecd066cb3d0a95fe177a8
Vikas Choubey <vikas.cby@gmail.com>
2020-09-26 20:29:10 +0530

    Updates

---
+++ precache-manifest.3653b1f4994a284f591d34f8c5d6d053.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb9312eddf511a9fcdb1075387c3f35",
+    "url": "/amazonclone/index.html"
+  },
+  {
+    "revision": "cdc58d8b6501c9838143",
+    "url": "/amazonclone/static/css/main.c6ae30d3.chunk.css"
+  },
+  {
+    "revision": "bc0ea08a5855a2b690e1",
+    "url": "/amazonclone/static/js/2.2faebba5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/amazonclone/static/js/2.2faebba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc58d8b6501c9838143",
+    "url": "/amazonclone/static/js/main.12e8454a.chunk.js"
+  },
+  {
+    "revision": "f4645a9177d8dc3d01ff",
+    "url": "/amazonclone/static/js/runtime-main.74fb3547.js"
+  },
+  {
+    "revision": "9e3bd55ad4cf2c4aba94713195a6146a",
+    "url": "/amazonclone/static/media/banner-image-min.9e3bd55a.jpg"
+  }
+]);
