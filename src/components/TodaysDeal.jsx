762b300c15138bd8e7a1b675855fe6be545aa00e
Vikas2426 <vikas.cby@gmail.com>
2021-02-14 22:33:36 +0530

    feat: add scroll buttons

--- src/components/TodaysDeal.jsx
+++ src/components/TodaysDeal.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './TodaysDeal.css'
 import list from '../assets/todaysDealItem'
 import DealEndsIn from '../assets/DealEndsIn'
 
 function TodaysDeal() {
+
+    const listRef = useRef();
+
+    const scrollLeft = () => {
+        listRef.current.scrollLeft -= 300;
+    }
+
+    const scrollRight = () => {
+        listRef.current.scrollLeft += 300;
+    }
+
     return (
         <div className='deals-container'>
+            <div className="scroller left-scroller" onClick={scrollLeft}> &#60; </div>
+
             <div className="deals-carousel">
                 Today's Deals
-                <ul className="deal-products-container">
+                    <ul className="deal-products-container" ref={listRef}>
+
                     {list.map(item => <li className="deal-product" key={item.imgUrl}>
 
                         <img className="deal-img" src={item.imgUrl} alt='deal-product' />
@@ -16,9 +30,10 @@ function TodaysDeal() {
                         <DealEndsIn />
 
                     </li>)}
-
                 </ul>
             </div>
+            <div className="scroller right-scroller" onClick={scrollRight}>  &#62; </div>
+
         </div>
 
     )
