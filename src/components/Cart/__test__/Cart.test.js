e6ff7c4c8f9fd5b3fc6a310cd99227f7079138ef
Vikas2426 <vikas.cby@gmail.com>
2022-07-14 10:13:41 +0530

    feat: add test cases fr Cart component

---
+++ src/components/Cart/__test__/Cart.test.js
@@ -0,0 +1,26 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import Cart from "..";
+
+jest.mock("../../Header", () => "header");
+
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  useContext: () => ({
+    items: [
+      {
+        imgUrl: "www.url.com",
+        price: 400,
+        quantity: 1,
+      },
+    ],
+  }),
+}));
+
+describe("Cart", () => {
+  it("should render without error", () => {
+    const { debug } = render(<Cart />);
+    debug();
+    expect(screen).toMatchSnapshot();
+  });
+});
