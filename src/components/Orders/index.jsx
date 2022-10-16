c9343d58615feb0d3e69a890e793413752ed48eb
Vikas Choubey <vikas.cby@gmail.com>
2022-10-16 18:34:19 +0530

    feat: add banner to orders page

--- src/components/Orders/index.jsx
+++ src/components/Orders/index.jsx
@@ -1,9 +1,11 @@
 import Header from "../Header";
+import banner from "../../assets/cart-banner.jpg";
 
 const Orders = () => {
   return (
     <>
       <Header />
+      <img className="cart-banner" src={banner} alt="cart-banner" />
       <h2 className="cart-heading">Your orders' list: is empty</h2>
       <h1 className="empty-cart-icon">
         <i class="fas fa-shopping-cart"></i>
