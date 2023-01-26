f6cc8f67f5ac43eb91b457bc0b6a8944fbcf014b
Vikas2426 <vikas.cby@gmail.com>
2023-01-26 20:18:51 +0530

    feat: secret

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,14 +1,15 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+
 const config = {
-  apiKey: "AIzaSyBPH1mjsUtqA9fkCNyyD7f1nESIEwRhCWc",
-  authDomain: "clone-81a36.firebaseapp.com",
-  databaseURL: "https://clone-81a36.firebaseio.com",
-  projectId: "clone-81a36",
-  storageBucket: "clone-81a36.appspot.com",
-  messagingSenderId: "652787402532",
-  appId: "1:652787402532:web:491bd8e9a414df6b5655b1",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_BKT,
+  messagingSenderId: process.env.REACT_APP_MSGSENDERID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(config);
